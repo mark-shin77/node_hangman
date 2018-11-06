1fca6cca6ab79fc361090eaa27130511b092e6a3
Mark <contact.mshin@gmail.com>
2018-11-05 17:29:44 -0700

    comments

--- letter.js
+++ letter.js
@@ -1,17 +1,25 @@
 var Letter = function(letter){
+    // Property to store letters
     this.letter = letter;
+    // Boolean to check whether or not letter has been guessed
     this.appear = false;
+    // Function to populate correct info to user
     this.letterGuesser = function (){
+        // Makes sure to not read the blanks 
         if(this.letter == " "){
             this.appear = true;
             return " ";
         }
+        // Returns an underscore if letter is not in the word
         if(this.appear = false){
             return "_ ";
-        } else {
+        } 
+        // Returns letter if it is part of the word
+        else {
             return this.letter
         }
     }
 }
 
+// Export to use in words.js
 module.exports = Letter;
--- word.js
+++ word.js
@@ -1,17 +1,21 @@
+// Calling to letter.js file
 var Letter = require("./letter");
 
 var Word = function (word){
+    // Storing the word
     this.word = word;
+    // Collecting all guessed letters
     this.letters = [];
+    // Boolean to check if the correct word has been guessed
     this.wordFound = false;
-
+    // Pushing Guessed letters to letters array
     this.pushLetters = function (){
         for (var x = 0; x < this.word.length; x++){
             var newLetter = new Letter (this.word[x])
             this.letters.push(newLetter)
         }       
     }
-
+    // If all letters are guessed correctly, run this function
     this.allLettersFound = function (){
         if(this.letters.every(function(ltr){
             return ltr.appear === true;
@@ -20,8 +24,29 @@ var Word = function (word){
             this.wordFound = true;
             return true;
         }
+    };
+    // Goes through each letter to check if it matches the guessed letter
+    this.checkIfLetterGuessed = function(guess){
+        var whatToReturn = 0;
+        this.letters.forEach(function(ltr){
+            if(ltr.letter === guess){
+                ltr.appear = true;
+                whatToReturn++;
+            }
+        })
+        // If guess matches Letter property, the letter shows
+        return whatToReturn;
     }
+    // Renders word based on letters found
+    this.wordRender = function (){
+        var display = "";
+        this.letter.forEach(function(ltr){
+            var currentLetter = ltr.letterRender();
+            display+=currentLetter;
+        })
 
+        return display;
+    }
 
 }
 
